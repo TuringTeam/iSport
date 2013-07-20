f250f67f27e918e92ca35b1bf58d735fc850031c
Young Shook <pumianwuya@163.com>
2013-07-20 15:02:49 +0800

    Initial Commit

---
+++ iSport/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iSport
+//
+//  Created by YoungShook on 13-7-20.
+//  Copyright (c) 2013年 qfpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
