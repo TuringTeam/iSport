de15538eab23dc8035f7f00536a6b9423406027d
Young Shook <pumianwuya@163.com>
2013-07-20 21:58:21 +0800

    update Model
    Signed-off-by: Young Shook <pumianwuya@163.com>

--- iSport/sportMessage.h
+++ iSport/sportMessage.h
@@ -9,11 +9,14 @@
 #import <Foundation/Foundation.h>
 
 @interface sportMessage : NSObject
-@property(nonatomic,retain) NSString *ID;
-@property(nonatomic,retain) NSString *title;
-@property(nonatomic,retain) NSString *time;
-@property(nonatomic,retain) NSString *address;
+@property(nonatomic,retain) NSString *ID;   //消息ID
+@property(nonatomic,retain) NSString *message;   //消息内容
+@property(nonatomic,retain) NSString *pubTimeDate;    //发布时间
+@property(nonatomic,retain) NSString *pubTimeStr; //发布时间 例如:36分钟前
+@property(nonatomic,retain) NSDate *endTimeDate;    //活动结束时间
+@property(nonatomic,retain) NSString *endTimeDataStr; //距离结束时间 例如:1小时40分钟后
+@property(nonatomic,retain) NSString *address;  //活动地址
 @property(nonatomic,retain) NSString *remarks; //备注
-@property(nonatomic,retain) UIImage *headImage;
+@property(nonatomic,retain) UIImage *headImage;  //用户头像
 
 @end
