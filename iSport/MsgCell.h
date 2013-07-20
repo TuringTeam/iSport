1db22d38fc5fcbfce16c6e77d0030e0f06d2ebe0
Young Shook <pumianwuya@163.com>
2013-07-21 05:30:42 +0800

    Restore miss file
    Signed-off-by: Young Shook <pumianwuya@163.com>

---
+++ iSport/MsgCell.h
@@ -0,0 +1,13 @@
+//
+//  MsgCell.h
+//  iSport
+//
+//  Created by YoungShook on 13-7-20.
+//  Copyright (c) 2013年 qfpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MsgCell : UITableViewCell
+
+@end
